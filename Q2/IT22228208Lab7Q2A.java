edb8533b2ed4e4b95852d5181139b7e5804a99b1
IT22228208 <It22228208@my.sliit.lk>
2026-09-23 14:52:30 +0530

    Add files via upload

---
+++ Q2/IT22228208Lab7Q2A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class IT22228208Lab7Q2A {
+    public static void main(String[] args) {
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 5; col++) {
+                System.out.print("$ ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Q2/IT22228208Lab7Q2C.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class IT22228208Lab7Q2C {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
